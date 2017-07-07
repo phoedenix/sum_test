ec12f94a357796139f7eb08f7ca30679a6553549
phoedenix <root@localhost.localdomain>
2017-07-06 19:13:52 -0800

    Add sum modules

---
+++ sum.c
@@ -0,0 +1,9 @@
+#include "sum.h"
+int sum(int n) {
+	int res = 0;
+	int i;
+	for (int i = 1; i<=n; i++)
+		res += i;
+	return res;
+}
+
---
+++ sum.h
@@ -0,0 +1,5 @@
+#ifdef __SUM_H__
+#define __SUM_H__
+int sum(int n);
+#endif
+
